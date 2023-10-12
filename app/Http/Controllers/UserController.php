df6da6e4a7809b7a8bc3c1476727e73b6af26957
safi-okz <safi90655@gmail.com>
2023-10-12 15:07:51 +0500

    dynamic data of post

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
         $user = User::find($id);
         if($user === null) {
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -16,10 +16,10 @@ let openOverlay = ref(false);
 
 const props = defineProps({
     posts: Object,
-    allUser: Object
+    allUsers: Object
 });
 
-const { posts, allUser } = toRefs(props);
+const { posts, allUsers } = toRefs(props);
 
 onMounted(() => {
     window.addEventListener('resize', () => {
@@ -31,7 +31,7 @@ onMounted(() => {
 <template>
     <Head title="Instagram" />
     <MainLayout>
-        {{ posts }}
+        <!-- {{ posts }} -->
         <div class="mx-auto lg:pl-0 md:pl-[80px] pl-0">
             <Carousel
                     v-model="currentSlide"
@@ -42,16 +42,16 @@ onMounted(() => {
                     :itemsToShow="9" :wrapAround="true"
 
             >
-                <Slide v-for="slide in 20" :key="slide">
-                    <Link href="/" class="relative mx-auto text-center mt-4 px-2 cursor-pointer">
+                <Slide v-for="slide in allUsers" :key="slide">
+                    <Link :href="route('user.show', {id: slide.id})" class="relative mx-auto text-center mt-4 px-2 cursor-pointer">
                         <div class="z-[-1] flex w-[64px] rounded-full rotate-45 h-[64px] bg-red contrast-[1.3] bg-gradient-to-t from-yellow-300 to-purple-200 via-red-500">
                             <div class="rounded-full flex ml-[3px] mt-[3px] mb-[3px] w-[58px] h-[58px] bg-white">
 
-                                <img :src="`https://picsum.photos/id/${slide}/300/320`" alt="" class="rounded-full w-[58px] h-[58px]">
+                                <img :src="slide.file" alt="" class="rounded-full w-[58px] h-[58px]" />
                             </div>
                         </div>
                         <div class="text-xs mt-2 w-[60px] turncate text-ellipsise overflow-hidden">
-                            Name here {{ slide }}
+                         {{ slide.name }}
                         </div>
                     </Link>
                 </Slide>
@@ -61,34 +61,37 @@ onMounted(() => {
                 </template>
             </Carousel>
 
-            <div id="Posts" class="px-4 max-w-[600px] mx-auto mt-10">
+            <div id="Posts" v-for="post in posts.data" :key="post" class="px-4 max-w-[600px] mx-auto mt-10">
                 <div class="flex justify-between items-center py-2">
                     <div class="flex items-center">
-                        <Link href="/" class="flex items-center">
-                            <img src="https:/picsum.photos/id/54/300/320" alt="" class="rounded-full h-[38px] h-[38px]">
-                            <div class="ml-4 font-extrabold text-[15px]">Name here</div>
+                        <Link :href="route('user.show', {id: post.user.id})" class="flex items-center">
+                            <img :src="post.user.file" alt="" class="rounded-full h-[38px] h-[38px]">
+                            <div class="ml-4 font-extrabold text-[15px]">{{ post.user.name }}</div>
                         </Link>
                         <div class="flex items-center text-[15px] text-gray-500">
                             <span class="-mt-5 ml-2 mr-[5px] text-[35px]">.</span>
-                            <div>DATE HERE</div>
+                            <div>{{ post.created_at }}</div>
                         </div>
                     </div>
 
                     <DotsHorizontal class="cursor-pointer" :size="27" />
                 </div>
                 <div class="bg-black rounded-lg w-full flex items-center min-h-[400px]">
-                    <img src="https:/picsum.photos/id/54/300/320" alt="" class="mx-auto w-full">
+                    <img :src="post.file" alt="" class="mx-auto w-full">
                 </div>
 
-                <LikeSection />
-                <div class="text-black font-extrabold py-1">3 Likes</div>
+                <LikeSection
+                            :post="post"
+                            @like="updateLike($event)"
+                 />
+                <div class="text-black font-extrabold py-1">{{ post.liekes.length }} Likes</div>
 
                 <div>
-                    <span class="text-black font-extrabold">Name Here</span>
-                    this is some text here
+                    <span class="text-black font-extrabold">{{ post.user.name }}</span>
+                    {{ post.text }}
                 </div>
-                <button @click="openOverlay = true" class="text-gray-500 font-extrabold py-1">
-                    View all 4 commnets
+                <button @click="currentPost = post; openOverlay = true" class="text-gray-500 font-extrabold py-1">
+                    View all {{ post.comments.length }} commnets
                 </button>
             </div>
 
